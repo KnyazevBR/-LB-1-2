3d2b5717dede2f54248b0cd4aa28ea46d1a60a53
KnyazevBR <145508470+KnyazevBR@users.noreply.github.com>
2023-09-20 01:50:45 +0300

    Add files via upload

---
+++ блок 2 задача 1(7).pas
@@ -0,0 +1,8 @@
+﻿program two;
+var x, a: integer;
+begin
+  write ('Введите число: ');
+  read (x);
+  a:= x mod 10;
+  write ('Последняя цифра: ', a);
+end.
---
+++ блок 2 задача 5(11).pas
@@ -0,0 +1,12 @@
+﻿program chee;
+var a, b, c, d, raz, tn: integer;
+begin
+  writeln ('Введите трехзначное число: ');
+  read (tn);
+  a:= tn div 100;
+  b:= tn div 10 mod 10;
+  c:= tn mod 10;
+  d:= c*100 + b*10 + a;
+  raz:= tn - d;
+  writeln (raz);
+end.
